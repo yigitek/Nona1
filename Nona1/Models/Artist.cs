8da509ccd9d76f0cc9a2899615107d59dae20556
yigitek <yigitek@gmail.com>
2024-01-26 01:41:21 +0300

    Add project files.

---
+++ Nona1/Models/Artist.cs
@@ -0,0 +1,10 @@
+﻿namespace Nona1.Models
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Item> Items { get; set; } = new List<Item>();
+    }
+}
---
+++ Nona1/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace Nona1.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string ImageUrl { get; set; }
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
+    }
+}
