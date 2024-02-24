ca9b6794a9cc482ac8e6014a7f1657bababf8100
yigitek <yigitek@gmail.com>
2024-02-24 18:06:04 +0100

    sagane

---
+++ Nona1/DTOs/anan.cs
@@ -0,0 +1,13 @@
+﻿namespace Nona1.DTOs
+{
+    public class anan
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        //public List<Item>? Items { get; set; } = new List<Item>();
+        //public List<Collab>? Collabs { get; set; } = new List<Collab>();
+
+    }
+}
